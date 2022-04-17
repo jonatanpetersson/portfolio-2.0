edd297061a383bfce8c61f9a69936cab615627ac
Jonatan Petersson <jonatan@climateview.global>
2022-04-17 12:48:46 +0200

    Removed weld

---
+++ src/components/Home.js
@@ -0,0 +1,3 @@
+const descriptionElement = document.querySelector('home-description');
+
+descriptionElement.textContent = `Hi, I'm Jonatan. Full stack developer based in Stockholm, Sweden. I work with ClimateView via </salt>.`
