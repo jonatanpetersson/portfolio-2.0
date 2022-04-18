0bd1df7f817310a4e368e7fe09eda315d81b748e
Jonatan Petersson <jonatan@climateview.global>
2022-04-18 15:47:44 +0200

    aaa

--- src/components/Home.js
+++
@@ -1,3 +0,0 @@
-const descriptionElement = document.querySelector('home-description');
-
-descriptionElement.textContent = `Hi, I'm Jonatan. Full stack developer based in Stockholm, Sweden. I work with ClimateView via </salt>.`
