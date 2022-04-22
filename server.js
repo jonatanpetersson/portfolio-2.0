be047b337d9b340f5318efb890ade3bd3535e184
Jonatan Petersson <jonatan@climateview.global>
2022-04-22 16:01:58 +0200

    Some mobile view improvements

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-express().use(express.static('dist')).get('/events', (_req, res) => {
-  res.set({
-    'Cache-Control': 'no-cache',
-    'Content-Type': 'text/event-stream',
-    'Connection': 'keep-alive'
-  });
-  res.flushHeaders();
-  res.write('hellooo');
-} 
-).listen(1337, () => console.log('Listening on port 1337'));
+express()
+  .use(express.static('dist'))
+  .get('/events', (_req, res) => {
+    res.set({
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive'
+    })
+    res.flushHeaders();
+    res.write('hellooo');
+  })
+  .listen(4200, () => console.log('Listening on port 4200'));
